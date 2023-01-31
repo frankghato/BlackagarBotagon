940aa893146e8447099d75f547d13a3dfb58418b
frankghato <59925619+frankghato@users.noreply.github.com>
2023-01-30 21:59:10 -0500

    Update skipto.js

--- slash/skipto.js
+++ slash/skipto.js
@@ -7,19 +7,17 @@ module.exports = {
         .setName("skipto")
         .setDescription("Skips to specified track number.")
         .addNumberOption((option) => 
-            option.setName("tracknumber").setDescription("Track to skip to").setMinValue(1).setRequired(true)),
-    run: async ({client,interaction}) => {
-        const queue = client.player.getQueue(interaction.guildId)
-        if(!queue)
-        {
-            return await interaction.editReply("There are no songs in the queue.")
-        }
-        const trackNum = interaction.options.getNumber("trackNumber")
-        if(trackNum > queue.tracks.length)
-        {
-            return await interaction.editReply("Invalid track number.")
-        }
-        queue.skipTo(trackNum-1)
-        await interaction.editReply("Skipped ahead to track ${trackNum}");
-    },
+        option.setName("tracknumber").setDescription("The track to skip to").setMinValue(1).setRequired(true)),
+	run: async ({ client, interaction }) => {
+		const queue = client.player.getQueue(interaction.guildId)
+
+		if (!queue) return await interaction.editReply("There are no songs in the queue")
+
+        const trackNum = interaction.options.getNumber("tracknumber")
+        if (trackNum > queue.tracks.length)
+            return await interaction.editReply("Invalid track number")
+		queue.skipTo(trackNum - 2)
+
+        await interaction.editReply(`Skipped ahead to track number ${trackNum}`)
+	},
 }
